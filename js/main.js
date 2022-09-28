e0bbdd3e79dc538b63bc010b2463373387e0af55
CuBin2009 <ndat0113@gmail.com>
2022-09-29 00:00:02 +0700

    day 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-}
+    searchForm.classList.toggle('active');}
 
 let loginForm = document.querySelector('.login-form-container');
 
@@ -33,3 +32,23 @@ window.onload = () =>{
         document.querySelector('.header .header-2').classList.remove('active');
     }
 }
+
+var swiper = new swiper(".books-slider", {
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+    },
+    breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    768: {
+        slidesPerView: 2,
+    },
+    1024: {
+        slidesPerView: 3,
+    },
+    },
+});
